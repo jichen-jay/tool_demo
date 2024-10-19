6a832ee22f3549ed1ac1882eb1a19d84e0006ce4
jaykchen <jaykchen@icloud.com>
2024-10-19 12:22:19 -0400

    create_tool_with_function can create tool with tool_def_obj as str passed in at macro decoration

--- func_builder/src/lib.rs
+++ func_builder/src/lib.rs
@@ -1,101 +1,77 @@
-extern crate proc_macro;
-
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, FnArg, ItemFn, Pat, PatIdent, PatType};
+use syn::{parse_macro_input, ItemFn, Expr};
 
 #[proc_macro_attribute]
 pub fn create_tool_with_function(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    // Parse the function definition
+    // Parse the input tokens into a syntax tree
     let input_fn = parse_macro_input!(item as ItemFn);
+    let attr_expr = parse_macro_input!(_attr as Expr);
 
     // Extract the function name
-    let func_name = &input_fn.sig.ident;
+    let fn_name = &input_fn.sig.ident;
 
-    // Extract function arguments
+    // Extract argument names and types
+    let inputs = &input_fn.sig.inputs;
     let mut arg_names = Vec::new();
     let mut arg_types = Vec::new();
-    let mut arg_idents = Vec::new();
+    let mut arg_type_tokens = Vec::new();
 
-    for input in &input_fn.sig.inputs {
-        if let FnArg::Typed(PatType { pat, ty, .. }) = input {
-            // Get the argument name and identifier
-            if let Pat::Ident(PatIdent { ident, .. }) = &**pat {
-                arg_names.push(ident.to_string());
-                arg_idents.push(ident.clone());
+    for arg in inputs {
+        if let syn::FnArg::Typed(pat_type) = arg {
+            // Get the argument name
+            if let syn::Pat::Ident(pat_ident) = &*pat_type.pat {
+                let arg_name = &pat_ident.ident;
+                arg_names.push(quote! { #arg_name });
             }
             // Get the argument type
-            arg_types.push(ty.clone());
+            let arg_type = &*pat_type.ty;
+            arg_types.push(quote! { #arg_type }.to_string());
+            arg_type_tokens.push(arg_type.clone());
         }
     }
 
-    let func_tool_name_str = format!("{}_tool", func_name);
-    let func_tool_name = syn::Ident::new(&func_tool_name_str, func_name.span());
-
-    // Generate the code
-    let expanded = quote! {
-        // Original function definition
+    // Generate the code to create the Tool struct
+    let gen = quote! {
         #input_fn
 
-        // Function to get the Tool instance
-        pub fn #func_tool_name() -> Tool {
-            use std::sync::Arc;
-            use std::error::Error;
+        fn create_tool() -> Tool {
+            let arg_names = vec![#(stringify!(#arg_names).to_string()),*];
+            let arg_types = vec![#(stringify!(#arg_type_tokens)),*];
 
-            let arg_names = vec![#(stringify!(#arg_idents).to_string()),*];
-            let arg_types = vec![#(stringify!(#arg_types).to_string()),*];
+            let func = {
+                use std::sync::Arc;
+                let func = Arc::new(move |args: &[SupportedType]| -> MyResult<String> {
+                    let parsers = get_parsers();
 
-            let function = Arc::new(move |args: &[SupportedType]| -> Result<String, Box<dyn Error>> {
-                let parsers = get_parsers();
-                let mut iter = args.iter();
+                    let mut iter = args.iter();
+                    #(
+                        let #arg_names = {
+                            let arg = iter.next().ok_or("Not enough arguments")?.clone();
+                            let parser = parsers.get(#arg_types).ok_or("Parser not found")?;
+                            let any_val = parser(arg)?;
+                            let val = any_val.downcast::<#arg_type_tokens>().map_err(|_| "Type mismatch")?;
+                            *val
+                        };
+                    )*
 
-                // For storing temporary Strings to ensure they live long enough
-                let mut temp_strings: Vec<String> = Vec::new();
-
-                // Extract arguments
-                #(
-                    let #arg_idents: #arg_types = {
-                        let arg = iter.next().ok_or("Not enough arguments")?;
-                        let arg_type_str = stringify!(#arg_types);
-                        let parser = parsers.get(arg_type_str)
-                            .ok_or_else(|| format!("Parser not found for type {}", arg_type_str))?;
-                        let boxed_value = parser(arg)?;
-
-                        // Handle the downcasting based on the expected type
-                        if arg_type_str == "&str" {
-                            let string_value = boxed_value.downcast_ref::<String>()
-                                .ok_or("Type mismatch when casting to String")?.clone();
-                            temp_strings.push(string_value);
-                            temp_strings.last().unwrap().as_str()
-                        } else if arg_type_str == "String" {
-                            boxed_value.downcast_ref::<String>()
-                                .ok_or("Type mismatch when casting to String")?.clone()
-                        } else if arg_type_str == "i32" {
-                            *boxed_value.downcast_ref::<i32>()
-                                .ok_or("Type mismatch when casting to i32")?
-                        } else if arg_type_str == "f32" {
-                            *boxed_value.downcast_ref::<f32>()
-                                .ok_or("Type mismatch when casting to f32")?
-                        } else if arg_type_str == "bool" {
-                            *boxed_value.downcast_ref::<bool>()
-                                .ok_or("Type mismatch when casting to bool")?
-                        } else {
-                            return Err(format!("Unsupported type: {}", arg_type_str).into());
-                        }
-                    };
-                )*
-
-                #func_name(#(#arg_idents),*).map_err(|e| e.into())
-            }) as Arc<dyn Fn(&[SupportedType]) -> Result<String, Box<dyn Error>> + Send + Sync>;
+                    #fn_name(#(#arg_names),*)
+                }) as Arc<dyn Fn(&[SupportedType]) -> MyResult<String> + Send + Sync>;
+                func
+            };
 
             Tool {
-                name: stringify!(#func_tool_name).to_string(),
-                function,
-                arg_names,
-                arg_types,
+                name: serde_json::from_str::<serde_json::Value>(#attr_expr).unwrap()["name"]
+                    .as_str()
+                    .unwrap()
+                    .to_string(),
+                function: func,
+                tool_def_obj: #attr_expr,
+                arg_names: vec![#(stringify!(#arg_names).to_string()),*],
+                arg_types: vec![#(stringify!(#arg_type_tokens).to_string()),*],
             }
         }
     };
 
-    TokenStream::from(expanded)
+    gen.into()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,79 +2,12 @@ use serde_json::Value;
 use std::collections::HashMap;
 use std::error::Error;
 use std::sync::Arc;
+use anyhow::Result;
 
-type MyResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+use func_builder::create_tool_with_function; // Replace `your_crate_name` with the actual name
 
-#[macro_export]
-macro_rules! create_tool_with_function {
-    (
-        fn $func_name:ident($($arg_name:ident : $arg_type:ty),*) -> $ret_type:ty,
-        $TOOL_DEF_OBJ:expr
-    ) => {{
-        let arg_names = vec![$(stringify!($arg_name).to_string()),*];
-        let arg_types = vec![$(stringify!($arg_type).to_string()),*];
+type MyResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-        let func = Arc::new(move |args: &[SupportedType]| -> MyResult<String> {
-            let parsers = get_parsers();
-
-            let mut iter = args.iter();
-            $(
-                let $arg_name = {
-                    let arg = iter.next().ok_or("Not enough arguments")?.clone();
-                    let parser = parsers.get(stringify!($arg_type)).ok_or("Parser not found")?;
-                    let any_val = parser(arg)?;
-                    let val = any_val.downcast::<$arg_type>().map_err(|_| "Type mismatch")?;
-                    *val
-                };
-            )*
-
-            $func_name($($arg_name),*)
-        }) as Arc<dyn Fn(&[SupportedType]) -> MyResult<String> + Send + Sync>;
-
-        Tool {
-            name: serde_json::from_str::<Value>($TOOL_DEF_OBJ)
-                .unwrap()["name"]
-                .as_str()
-                .unwrap()
-                .to_string(),
-            function: func,
-            arg_names: arg_names,
-            arg_types: arg_types,
-        }
-    }};
-}
-
-fn get_current_weather(location: String, unit: String) -> MyResult<String> {
-    if location.contains("New") {
-        Ok(format!("Weather for {} in {}", location, unit))
-    } else {
-        Err(format!("Weather for {} in {}", location, unit).into())
-    }
-}
-
-pub const GET_WEATHER_TOOL_DEF_OBJ: &str = r#"
-        {
-            "name": "get_current_weather",
-            "description": "Get the current weather in a given location",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "location": {
-                        "type": "string",
-                        "description": "The city and state, e.g. San Francisco, CA"
-                    },
-                    "unit": {
-                        "type": "string",
-                        "enum": ["celsius", "fahrenheit"],
-                        "description": "The unit of measurement"
-                    }
-                },
-                "required": ["location", "unit"]
-            }
-        }
-        "#;
-
-// Implement SupportedType and parsers
 #[derive(Debug, Clone)]
 enum SupportedType {
     I32(i32),
@@ -83,6 +16,54 @@ enum SupportedType {
     String(String),
 }
 
+// Define Tool struct
+#[derive(Clone)]
+struct Tool {
+    name: String,
+    function: Arc<dyn Fn(&[SupportedType]) -> MyResult<String> + Send + Sync>,
+    tool_def_obj: &'static str,
+    arg_names: Vec<String>,
+    arg_types: Vec<String>,
+}
+
+impl Tool {
+    fn call(&self, arguments_w_val: Value) -> MyResult<String> {
+        let arguments = arguments_w_val
+            .as_object()
+            .ok_or("Invalid arguments format")?;
+        let mut ordered_vals = Vec::new();
+
+        for (i, arg_name) in self.arg_names.iter().enumerate() {
+            if let Some(arg_value) = arguments.get(arg_name) {
+                let arg_str = arg_value.as_str().ok_or("Invalid argument value")?;
+                let parsed_arg = parse_argument(&self.arg_types[i], arg_str);
+                ordered_vals.push(parsed_arg);
+            } else {
+                return Err(format!("Missing argument: {}", arg_name).into());
+            }
+        }
+
+        (self.function)(&ordered_vals)
+    }
+}
+
+// Parser functions
+fn parse_argument(arg_type: &str, arg_value: &str) -> SupportedType {
+    match arg_type {
+        "i32" => SupportedType::I32(arg_value.parse::<i32>().expect("Expected i32 for argument")),
+        "f32" => SupportedType::F32(arg_value.parse::<f32>().expect("Expected f32 for argument")),
+        "bool" => SupportedType::Bool(match arg_value {
+            "true" => true,
+            "false" => false,
+            _ => panic!("Expected bool for argument"),
+        }),
+        "String" => SupportedType::String(arg_value.to_string()),
+        "& str" => SupportedType::String(arg_value.to_string()),
+        _ => panic!("Invalid type"),
+    }
+}
+
+// Get parsers
 type ParserFn = dyn Fn(SupportedType) -> MyResult<Box<dyn std::any::Any>> + Send + Sync;
 
 fn get_parsers() -> HashMap<&'static str, Box<ParserFn>> {
@@ -133,7 +114,7 @@ fn get_parsers() -> HashMap<&'static str, Box<ParserFn>> {
     );
 
     parsers.insert(
-        "&str",
+        "& str",
         Box::new(|v| {
             if let SupportedType::String(val) = v {
                 Ok(Box::new(val.to_string()) as Box<dyn std::any::Any>)
@@ -146,81 +127,51 @@ fn get_parsers() -> HashMap<&'static str, Box<ParserFn>> {
     parsers
 }
 
-fn parse_i32(arg_value: &str) -> i32 {
-    arg_value.parse::<i32>().expect("Expected i32 for argument")
-}
-
-fn parse_f32(arg_value: &str) -> f32 {
-    arg_value.parse::<f32>().expect("Expected f32 for argument")
-}
-
-fn parse_bool(arg_value: &str) -> bool {
-    match arg_value {
-        "true" => true,
-        "false" => false,
-        _ => panic!("Expected bool for argument"),
+#[create_tool_with_function(GET_WEATHER_TOOL_DEF_OBJ)]
+fn get_current_weather(location: String, unit: String) -> MyResult<String> {
+    if location.contains("New") {
+        Ok(format!("Weather for {} in {}", location, unit))
+    } else {
+        Err(format!("Weather for {} in {}", location, unit).into())
     }
 }
 
-fn parse_string(arg_value: &str) -> String {
-    arg_value.to_string()
-}
-
-fn parse_argument(arg_type: &str, arg_value: &str) -> SupportedType {
-    match arg_type {
-        "i32" => SupportedType::I32(parse_i32(arg_value)),
-        "f32" => SupportedType::F32(parse_f32(arg_value)),
-        "bool" => SupportedType::Bool(parse_bool(arg_value)),
-        "String" | "&str" => SupportedType::String(parse_string(arg_value)),
-        _ => panic!("Invalid type"),
-    }
-}
-
-// Define the Tool struct with Arc and derive Clone
-#[derive(Clone)]
-struct Tool {
-    name: String,
-    function: Arc<dyn Fn(&[SupportedType]) -> MyResult<String> + Send + Sync>,
-    arg_names: Vec<String>,
-    arg_types: Vec<String>,
-}
-
-impl Tool {
-    fn call(&self, arguments_w_val: Value) -> MyResult<String> {
-        let arguments = arguments_w_val
-            .as_object()
-            .ok_or("Invalid arguments format")?;
-        let mut ordered_vals = Vec::new();
-
-        for (i, arg_name) in self.arg_names.iter().enumerate() {
-            if let Some(arg_value) = arguments.get(arg_name) {
-                let arg_str = arg_value.as_str().ok_or("Invalid argument value")?;
-                let parsed_arg = parse_argument(&self.arg_types[i], arg_str);
-                ordered_vals.push(parsed_arg);
-            } else {
-                return Err(format!("Missing argument: {}", arg_name).into());
+pub const GET_WEATHER_TOOL_DEF_OBJ: &str = r#"
+{
+    "name": "get_current_weather",
+    "description": "Get the current weather in a given location",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "location": {
+                "type": "string",
+                "description": "The city and state, e.g. San Francisco, CA"
+            },
+            "unit": {
+                "type": "string",
+                "enum": ["celsius", "fahrenheit"],
+                "description": "The unit of measurement"
             }
-        }
-
-        (self.function)(&ordered_vals)
+        },
+        "required": ["location", "unit"]
     }
 }
+"#;
 
-// Demonstrate how to use the generated tool
 fn main() -> Result<(), Box<dyn Error>> {
-    let tool = create_tool_with_function!(
-        fn get_current_weather(location: String, unit: String) -> MyResult<String>,
-        GET_WEATHER_TOOL_DEF_OBJ
-    );
+    // Create the tool
+    let tool = create_tool();
+
+    // Simulate LLM output
     let llm_output = serde_json::json!({
         "location": "York, NY",
         "unit": "fahrenheit"
     });
 
-    match tool.call(llm_output) {
-        Ok(result) => println!("Result: {}", result),
-        Err(e) => eprintln!("Error: {e}"),
-    }
+    // Call the tool with the arguments
+    let result = tool.call(llm_output).expect("funccall");
+
+    println!("Result: {}", result);
 
     Ok(())
 }
